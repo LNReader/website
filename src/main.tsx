425a6dcb56867b11f5042750916124c8b9e4c6dd
nyagami <hoangquan05112002@gmail.com>
2024-01-27 15:43:52 +0700

    install mui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Root from './routes/root.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './error-page.tsx';
+import Docs from './routes/docs.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <Root/>, 
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: "docs",
+    element: <Docs/>,
+    errorElement: <ErrorPage/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
