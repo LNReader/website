f5f36752eebb70213b20f2a2525a1aacd557eef6
nyagami <hoangquan05112002@gmail.com>
2024-01-28 00:39:56 +0700

    add appbar

--- src/views/homepage/index.tsx
+++ src/views/homepage/index.tsx
@@ -6,44 +6,6 @@ export default function Homepage() {
     }, []);
     return (
         <>
-            <div id="sidebar">
-                <div>
-                    <form id="search-form" role="search">
-                        <input
-                            id="q"
-                            aria-label="Search contacts"
-                            placeholder="Search"
-                            type="search"
-                            name="q"
-                        />
-                        <div
-                            id="search-spinner"
-                            aria-hidden
-                            hidden={true}
-                        />
-                        <div
-                            className="sr-only"
-                            aria-live="polite"
-                        ></div>
-                    </form>
-                </div>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href={`https://github.com/LNReader/lnreader`}>Github</a>
-                        </li>
-                        <li>
-                            <a href={`/website/docs`}>Docs</a>
-                        </li>
-                        <li>
-                            <a href={`/website/tos`}>Terms of serice</a>
-                        </li>
-                        <li>
-                            <a href={`/website/privacy-policy`}>Privacy Policy</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
         </>
     );
 }
